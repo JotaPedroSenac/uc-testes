025dd0c016db1e3e0fcfddb6c9b7fb94fce24123
JOAO PEDRO DE SOUZA CORINGA <a95436285@rn.senac.local>
2025-07-08 17:10:01 -0300

    aula 04 de testes correcao

--- Aula04/atividade/questao01/validaSenha.test.js
+++ Aula04/atividade/questao01/validaSenha.test.js
@@ -1,22 +1,22 @@
 const validaSenha = require('./validaSenha');
 
-describe('criação dos testes de caixa branca', ()=>{
-    test('Caminho 01 (1 - 2 - False)', ()=>{
+describe('criação dos testes de caixa branca', () => {
+    test('Caminho 01 (1 - False)', () => {
         expect(validaSenha(123)).toBeFalsy();
     });
-    test('Caminho 02 (1 - 2 - 3 - False)', ()=>{
-        expect(validaSenha('oito')).toBeFalsy();
+    test('Caminho 02 (1 - 2 - False)', () => {
+        expect(validaSenha('oito')).toBeFalsy(); 
     });
-    test('Caminho 03 (1 - 2 - 3 - 4 - False)', ()=>{
-        expect(validaSenha('3333')).toBeFalsy();
+    test('Caminho 03 (1 - 2 - 3 - False)', () => {
+        expect(validaSenha('abcdefgh')).toBeFalsy();
     });
-    test('Caminho 04 (1 - 2 - 3 - 4 -5 - False)', ()=>{
-        expect(validaSenha('aaaa')).toBeFalsy();
+    test('Caminho 04 (1 - 2 - 3 - 4 - False)', () => {
+        expect(validaSenha('12345678')).toBeFalsy(); 
     });
-    test('Caminho 05 (1 - 2 - 3 - 4 -5 - 6 - False)', ()=>{
-        expect(validaSenha('aa 1a')).toBeFalsy();
+    test('Caminho 05 (1 - 2 - 3 - 4 - 5 - False)', () => {
+        expect(validaSenha('abc 1234')).toBeFalsy(); 
     });
-    test('Caminho 06 (1 - 2 - 3 - 4 -5 - 6 - true)', ()=>{
-        expect(validaSenha('a1Z5ba1Z5b')).toBeTruthy();
+    test('Caminho 06 (1 - 2 - 3 - 4 - 5 - 6 - True)', () => {
+        expect(validaSenha('abc12345')).toBeTruthy(); 
     });
-})
+});
